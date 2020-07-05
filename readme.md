7c0ac1438d46655ca341b27453d36c7e335f08ee
MyAeroCode <meteo_meteo@naver.com>
2020-07-05 21:16:08 +0900

    실습코드 실행환경 셋팅

---
+++ readme.md
@@ -0,0 +1,15 @@
+## Node-gRPC Study
+
+`TypeScript`와 함께 `gRPC`의 기능들을 공부합니다.
+
+---
+
+실습 파일을 실행하려면 다음 명령어를 입력해주세요 :
+
+```bash
+$ npm start CHAPTER_NUMBER [server | client]
+```
+
+| chapter | name |
+| :-----: | ---- |
+
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+async function bootstrap() {
+    try {
+        /**
+         * 두 자리 정수로 변환한다.
+         */
+        function formatNumber(num: number): string {
+            const prefix = num < 10 ? "0" : "";
+            return prefix + num;
+        }
+
+        /**
+         * 두자리 챕터 번호
+         */
+        const chapter = formatNumber(Number(process.argv[2]));
+
+        /**
+         * 서버 또는 클라이언트 구분.
+         */
+        const type = process.argv[3] === "server" ? "server" : "client";
+
+        /**
+         * 실행할 모듈의 상대 경로.
+         */
+        const targetModulePath = `./${chapter}/grpc-${type}`;
+
+        /**
+         * 실행할 모듈의 export default 값.
+         */
+        const { default: startModule } = await import(targetModulePath);
+
+        startModule();
+    } catch (e) {
+        console.error(e);
+    }
+}
+bootstrap();
